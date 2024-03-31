c25174583a58da07980a53e9a07ad26583bb3d7f
thesby <1099361041@qq.com>
2024-03-31 21:38:13 +0800

    first commit

---
+++ test_llava_qwen2moe.py
@@ -0,0 +1,14 @@
+from llava.model.builder import load_pretrained_model
+import ipdb
+
+
+if __name__ == '__main__':
+    model_name = 'llava-v1.6-qwen2moe'
+    # model_path = '/home/thesby/projects/pretrained_models/LLM/Qwen1.5-MoE-A2.7B-Chat-GPTQ-Int4'
+    model_path = '/home/thesby/projects/pretrained_models/LLM/Qwen1.5-MoE-A2.7B-Chat-GPTQ-Int4'
+
+    model = load_pretrained_model(model_name=model_name, model_path=model_path,
+                                  load_4bit=False, device='cuda', model_base=None)
+
+    ipdb.set_trace()
+    a = 0
